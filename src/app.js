c85c470d2714b5f02742f1590c552eeaa199b449
MariaApple17 <mariaappleroncales@gmail.com>
2026-02-28 20:32:43 +0800

    get back

--- src/app.js
+++ src/app.js
@@ -25,17 +25,10 @@ import skPlantillaRoutes from './routes/sk-plantilla.route.js';
 const app = express();
 
 /* ================= MIDDLEWARES ================= */
-import cors from "cors";
-
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://skfrontend-omega.vercel.app"
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 /* ================= AUTH & RBAC ================= */
 app.use("/api/auth", authRoutes);
